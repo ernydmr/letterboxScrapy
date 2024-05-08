1b367f673354c649cc1ab310c478d41a4f298247
ernydmr <eren8441@gmail.com>
2024-05-08 19:32:37 +0300

    v 1.0 -updated(movies are taking from a single link)

--- letterboxd/spiders/letterbox.py
+++ letterboxd/spiders/letterbox.py
@@ -1,36 +1,38 @@
 import scrapy
-
+import urllib.parse
 
 class LetterboxSpider(scrapy.Spider):
     name = "letterbox"
     allowed_domains = ["letterboxd.com"]
     start_urls = [
-        "https://letterboxd.com/film/harakiri/",
-        "https://letterboxd.com/film/come-and-see/",
-        "https://letterboxd.com/film/12-angry-men/",
-        "https://letterboxd.com/film/seven-samurai/",
-        "https://letterboxd.com/film/the-godfather-part-ii/",
-        "https://letterboxd.com/film/high-and-low/",
-        "https://letterboxd.com/film/parasite-2019/",
-        "https://letterboxd.com/film/the-human-condition-iii-a-soldiers-prayer/",
-        "https://letterboxd.com/film/the-godfather/",
-        "https://letterboxd.com/film/the-shawshank-redemption/"
+        "https://letterboxd.com/dave/list/official-top-250-narrative-feature-films/",
+        "https://letterboxd.com/dave/list/official-top-250-narrative-feature-films/page/2/",
+        "https://letterboxd.com/dave/list/official-top-250-narrative-feature-films/page/3/"
     ]
 
     def parse(self, response):
+        movie_elements = response.css('div.really-lazy-load')
+        for movie_element in movie_elements:
+            movie_url = movie_element.css('::attr(data-target-link)').extract_first()
+            if movie_url:
+                # Make sure the URL is absolute
+                movie_url = response.urljoin(movie_url)
+                yield scrapy.Request(url=movie_url, callback=self.parse_movie)
+
+    def parse_movie(self, response):
         movie = {}
+
+        tab_cast = response.css("#tab-cast")
+        castList = tab_cast.css("p a")
+
+        cast = []
+
         film_header = response.css(".film-header-group")
+
         movie["movieName"] = film_header.css(".headline-1.filmtitle .name::text").get().strip()
         movie["releaseYear"] = film_header.css(".releaseyear a::text").get()
         movie["directorName"] = film_header.css(".directorlist a span::text").get()
 
-        rating_header = response.css(".average-rating")
-        movie["rating"] = rating_header.css("a::text").get()
-
-        stats_header = response.css(".film-stats")
-        statsList = stats_header.css(".stat .filmstat-watches")
-
-        movie["watched"] = response.css('a.has-icon.tooltip::text').get()
 
         movie_info_html = response.css('.review.body-text .truncate p')
         if movie_info_html:
@@ -39,15 +41,20 @@ class LetterboxSpider(scrapy.Spider):
         else:
             movie["info"] = None
 
-        tab_cast = response.css("#tab-cast")
-        castList = tab_cast.css("p a")
-        cast = []
+        rating_label = response.xpath("//meta[@name='twitter:label2']/@content").get()
+        rating_data = response.xpath("//meta[@name='twitter:data2']/@content").get()
+
+        if rating_label == "Average rating" and rating_data:
+            movie["rating"] = rating_data.strip()
+
         for castP in castList:
             cast_item = {}
             cast_item["castRealName"] = castP.css("::text").get()
             cast_item["castActorpage"] = castP.attrib.get("href", "")
             cast_item["character"] = castP.attrib.get("title", "")
             cast.append(cast_item)
+
         movie["Cast List"] = cast
+        movie["movie_url"] = response.url
 
         yield movie
